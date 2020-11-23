af2cb3f8277a7ce1ad2815b34eb4a3e06d23c266
limheonjun <jumong40@naver.com>
2020-11-23 20:52:35 +0900

    UserDto 코드 작성

--- src/main/java/com/mhelper/dto/UserDto.java
+++ src/main/java/com/mhelper/dto/UserDto.java
@@ -1,5 +1,33 @@
 package com.mhelper.dto;
 
+import com.mhelper.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
-    //TODO
+
+    // 이메일
+    @NotEmpty
+    private String email;
+
+    // 비밀번호
+    @NotEmpty
+    private String password;
+
+    // 이름
+    private String name;
+
+    public UserDto(User user) {
+        this.email = user.getEmail();
+        this.name = user.getName();
+    }
+
 }
