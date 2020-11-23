2eb774577bf7795475b724886e87b60a163ed839
limheonjun <jumong40@naver.com>
2020-11-23 20:52:20 +0900

    이메일로 유저 아이디 찾는 메소드 추가

--- src/main/java/com/mhelper/repository/UserRepository.java
+++ src/main/java/com/mhelper/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.mhelper.repository;
 import com.mhelper.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
 }
